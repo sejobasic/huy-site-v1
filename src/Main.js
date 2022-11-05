eab9dd8c87b396ce656a4a76aae1ffc6cd924dfe
Sejo Basic <sejoob@gmail.com>
2022-11-05 15:27:11 -0700

    Init all pages and components - import react-router

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,39 @@
-import './Main.css';
+import './Main.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+// Components
+import Navbar from './components/Navbar'
+
+// Pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Releases from './pages/Releases'
+import Contact from './pages/Contact'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <Router>
+      <div className='main-container'>
+        <Navbar />
+        <div className="content-container">
+          <Routes>
+            <Route path='/'>
+              <Home />
+            </Route>
+            <Route path='/About'>
+              <About />
+            </Route>
+            <Route path='/Releases'>
+              <Releases />
+            </Route>
+            <Route path='/Contact'>
+              <Contact />
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from "react-dom/client";
+
+function Navbar() {
+  return (
+    <div className='nav-container'>
+      <nav>
+
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
